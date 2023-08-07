bc9cdcfc4649506c90512b288789fe2596b17990
Pablo Mercado <105954422+primercado@users.noreply.github.com>
2023-08-07 01:14:53 -0300

    agregué 2 funciones en control.py: obtener_coordenadas y obtener_clima y 2 rutas de flask en app.py

--- control.py
+++ control.py
@@ -1,6 +1,10 @@
 from flask import jsonify
 from sqlalchemy import text
 from database import db
+import requests
+
+API_KEY = '1e2566e8d350e5d5a0c41ea6c5eacb8a'
+
 
 def get_ciudad(ciudad):
     """Obtener datos del tiempo por nombre de ciudad"""
@@ -58,3 +62,51 @@ def obtener_datos_clima():
     } for row in rows]
 
     return data
+
+
+
+def obtener_coordenadas(nombre_ciudad, codigo_estado=None, codigo_pais=None, limite=5):
+    url_base = "http://api.openweathermap.org/geo/1.0/direct?q="
+    query = nombre_ciudad
+    if codigo_estado:
+        query += f",{codigo_estado}"
+    if codigo_pais:
+        query += f",{codigo_pais}"
+    url = f"{url_base}{query}&limit={limite}&appid={API_KEY}"
+
+    respuesta = requests.get(url)
+    if respuesta.status_code == 200:
+        resultados = respuesta.json()
+        if resultados:
+            coordenadas = [(resultado['name'], resultado['lat'], resultado['lon']) for resultado in resultados]
+            return coordenadas
+        else:
+            print(f"No se encontraron resultados para la ciudad {nombre_ciudad}.")
+            return None
+    else:
+        print(f"Error en la llamada a la API: {respuesta.status_code}")
+        return None
+    
+
+
+def obtener_clima_actual(nombre_ciudad, codigo_estado=None, codigo_pais=None, limite=5, exclude=None, units="metric", lang="es"):
+    coordenadas = obtener_coordenadas(nombre_ciudad, codigo_estado, codigo_pais, limite)
+    if coordenadas:
+        latitud, longitud = coordenadas[0][1], coordenadas[0][2]
+        url_clima = "https://api.openweathermap.org/data/3.0/onecall?"
+        url = f"{url_clima}lat={latitud}&lon={longitud}&appid={API_KEY}"
+        if exclude:
+            url += f"&exclude={exclude}"
+        url += f"&units={units}&lang={lang}"
+
+        respuesta = requests.get(url)
+        if respuesta.status_code == 200:
+            datos_clima = respuesta.json()
+            return datos_clima
+        else:
+            print(f"Error en la llamada a la API: {respuesta.status_code}")
+            return None
+    else:
+        print(f"No se encontraron coordenadas para la ciudad {nombre_ciudad}.")
+        return None
+    
